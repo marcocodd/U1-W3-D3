54925b978a38483f676e9293a8eb0598c8eb861e
marcocodd <brunetti.mrco@gmail.com>
2023-12-06 16:37:20 +0100

    first commit

---
+++ script/javascript.js
@@ -0,0 +1,28 @@
+const addList = function () {
+  const inputList = document.getElementById("input");
+  console.log(inputList.value);
+
+  const newLi = document.createElement("li");
+
+  console.log(newLi);
+
+  newLi.innerText = inputList.value;
+
+  const uList = document.getElementById("ul");
+
+  uList.appendChild(newLi);
+
+  newLi.classList.add("liStyleCursor");
+
+  const hiddenButton = document.createElement("button");
+  hiddenButton.innerText = "Delete";
+  newLi.appendChild(hiddenButton);
+  hiddenButton.addEventListener("click", function () {
+    hiddenButton.parentElement.remove();
+  });
+
+  newLi.addEventListener("click", function () {
+    newLi.classList.toggle("liStyle");
+  });
+  inputList.value = " ";
+};
